df6cf983b37af45969fa01572cd0da0ce529a8be
Ng Wing Tat, David <i@ngwingt.at>
2019-11-01 12:01:23 +0800

    ⬆️ Upgrade AuthCore RN module

--- metro.config.js
+++ metro.config.js
@@ -23,7 +23,7 @@ module.exports = (async () => {
     },
     resolver: {
       assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"]
+      sourceExts: [...sourceExts, "svg", "js", "jsx"]
     }
   }
 })()
