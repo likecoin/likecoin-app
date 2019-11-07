cdafde751a3a42537be1c5d29a914f55cd3d8541
Ng Wing Tat, David <i@ngwingt.at>
2019-11-07 18:38:32 +0800

    🍱 Add arrow up & arrow down icons

--- app/components/icon/icons/index.ts
+++ app/components/icon/icons/index.ts
@@ -1,8 +1,13 @@
+import ArrowDownIcon from "./arrow-down.svg"
 import ArrowLeftIcon from "./arrow-left.svg"
+import ArrowUpIcon from "./arrow-up.svg"
 import CrossIcon from "./cross.svg"
 import ShareIcon from "./share.svg"
 
 export const icons = {
+  "arrow-down": ArrowDownIcon,
+  "arrow-left": ArrowLeftIcon,
+  "arrow-up": ArrowUpIcon,
   back: ArrowLeftIcon,
   close: CrossIcon,
   share: ShareIcon,
