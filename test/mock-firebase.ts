5bdf22f3359bf33132c7754e566aaa6c4510df76
William Chong <williamchong007@msn.com>
2020-01-20 19:42:10 +0800

    ✅ Fix test missing firebase app mock

--- test/mock-firebase.ts
+++ test/mock-firebase.ts
@@ -1,2 +1,9 @@
-jest.mock('@react-native-firebase/crashlytics', () => {}, { virtual: true })
 jest.mock('@react-native-firebase/analytics', () => {}, { virtual: true })
+jest.mock('@react-native-firebase/crashlytics', () => {}, { virtual: true })
+jest.mock('@react-native-firebase/app', () => {
+  return {
+    app: jest.fn(() => ({
+      utils: jest.fn(() => ({})),
+    })),
+  }
+}, { virtual: true })
