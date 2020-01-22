1c6255c4cf1d25fa443bb7d185318dcc27181e78
Ng Wing Tat, David <i@ngwingt.at>
2020-01-22 10:23:19 +0800

    🐛 Fix unable to scroll in validator input screen

--- app/screens/staking-redelegation-validator-input-screen/staking-redelegation-validator-input-screen.style.ts
+++ app/screens/staking-redelegation-validator-input-screen/staking-redelegation-validator-input-screen.style.ts
@@ -2,14 +2,11 @@ import { StyleSheet, ViewStyle } from "react-native"
 import { spacing } from "../../theme"
 
 export const StakingRedelegationValidatorInputScreenStyle = StyleSheet.create({
-  Body: {
-    flexGrow: 1,
-  } as ViewStyle,
   Header: {
     alignItems: "flex-start",
   } as ViewStyle,
   Screen: {
-    flex: 1,
+    flexGrow: 1,
     padding: spacing[4],
     paddingTop: spacing[0],
   } as ViewStyle,
