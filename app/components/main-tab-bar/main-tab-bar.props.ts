9ba12b6aab8dd0b201efefe736e6a5ad21ddeaa9
Ng Wing Tat, David <i@ngwingt.at>
2020-01-31 01:54:23 +0800

    💄 Replace settings tab bar icon to user avatar

--- app/components/main-tab-bar/main-tab-bar.props.ts
+++ app/components/main-tab-bar/main-tab-bar.props.ts
@@ -1,5 +1,8 @@
 import { TabBarIconProps } from "react-navigation"
 
+import { User } from "../../models/user"
+
 export interface MainTabBarIconProps extends TabBarIconProps {
   routeName: string
+  user?: User
 }
--- app/components/main-tab-bar/main-tab-bar.tsx
+++ app/components/main-tab-bar/main-tab-bar.tsx
@@ -1,16 +1,24 @@
 import * as React from "react"
+import { inject, observer } from "mobx-react"
 
 import { MainTabBarIconProps } from "./main-tab-bar.props"
 
 import { Icon, IconTypes } from "../icon"
 
+import { RootStore } from "../../models/root-store"
 import { color } from "../../theme"
+import { Avatar } from "../avatar"
 
+@inject((rootStore: RootStore) => ({
+  user: rootStore.userStore.currentUser,
+}))
+@observer
 export class MainTabBarIcon extends React.Component<MainTabBarIconProps> {
   render() {
+    const { focused, routeName, user } = this.props
     let name: IconTypes
     let size = 24
-    switch (this.props.routeName) {
+    switch (routeName) {
       case "Bookmark":
         name = "tab-bookmark"
         break
@@ -18,14 +26,26 @@ export class MainTabBarIcon extends React.Component<MainTabBarIconProps> {
         name = "tab-reader"
         size = 32
         break
-      case "Settings":
+      case "Settings": {
+        if (user) {
+          const { avatarURL: src, isCivicLiker } = user
+          return (
+            <Avatar
+              src={src}
+              size={28}
+              focused={focused}
+              isCivicLiker={isCivicLiker}
+            />
+          )
+        }
         name = "tab-settings"
         break
+      }
       case "Wallet":
         name = "tab-wallet"
         break
     }
-    const fill = this.props.focused ? color.palette.likeCyan : color.palette.lightGrey
+    const fill = focused ? color.palette.likeCyan : color.palette.lightGrey
     return (
       <Icon
         name={name}
