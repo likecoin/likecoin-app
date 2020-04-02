0275d7764d27d07e4714e0951585b5bef01fdbb0
William Chong <williamchong007@msn.com>
2020-04-03 00:09:24 +0800

    ✨ Send deep link app referrer when register/login

--- app/services/branch-io/branch-io.ts
+++ app/services/branch-io/branch-io.ts
@@ -6,6 +6,19 @@ export class BranchIO {
    */
   private params: any
 
+  private appReferrer: string
+
+  private handleAppReferrerEvent(params: any) {
+    if (!params) return undefined
+    if (params.event === 'app_referral' && params.referrer) {
+      if (params.referrer) {
+        this.appReferrer = params.referrer
+        return this.appReferrer
+      }
+    }
+    return undefined
+  }
+
   async setup() {
     branch.subscribe(({ error, params }) => {
       if (error) {
@@ -13,11 +26,19 @@ export class BranchIO {
         return
       }
       this.params = params
+      this.handleAppReferrerEvent(params)
     })
   }
 
   getBranchInstance() {
     return branch
+  }
+
+  async getAppReferrer() {
+    if (this.appReferrer) return this.appReferrer
+    const params = await this.getLatestParams()
+    const referrer = await this.handleAppReferrerEvent(params)
+    return referrer
   }
 
   getParams() {
