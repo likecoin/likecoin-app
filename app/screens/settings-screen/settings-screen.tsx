55076717668297423b41417bdfa0c25a3f356bd8
William Chong <williamchong007@msn.com>
2020-04-14 18:23:43 +0800

    💄 Add UI and config for app referral

--- app/screens/settings-screen/settings-screen.tsx
+++ app/screens/settings-screen/settings-screen.tsx
@@ -39,14 +39,16 @@ import * as Intercom from "../../utils/intercom"
 
 export interface SettingsScreenProps extends NavigationScreenProps<{}> {
   chain: ChainStore
-  userStore: UserStore
-  readerStore: ReaderStore
+  userStore: UserStore,
+  readerStore: ReaderStore,
+  isAppReferralEnabled: boolean,
 }
 
 @inject((allStores: any) => ({
   chain: allStores.chainStore as ChainStore,
   userStore: allStores.userStore as UserStore,
   readerStore: allStores.readerStore as ReaderStore,
+  isAppReferralEnabled: !!(allStores.rootStore as RootStore).env.appConfig.getValue("APP_REFERRAL_ENABLE"),
 }))
 @observer
 export class SettingsScreen extends React.Component<SettingsScreenProps, {}> {
@@ -82,6 +84,11 @@ export class SettingsScreen extends React.Component<SettingsScreenProps, {}> {
   private onPressFollowSettings = () => {
     logAnalyticsEvent('SettingsClickFollowSettings')
     this.props.navigation.navigate("FollowSettings")
+  }
+
+  private onPressReferral = () => {
+    logAnalyticsEvent('SettingsClickReferral')
+    this.props.navigation.navigate("Referral")
   }
 
   private onPressQRCodeButton = () => {
@@ -189,6 +196,15 @@ export class SettingsScreen extends React.Component<SettingsScreenProps, {}> {
             style={SETTINGS_MENU.TABLE_CELL_FIRST_CHILD}
             onPress={this.onPressFollowSettings}
           />
+          {this.props.isAppReferralEnabled &&
+            <Button
+              preset="plain"
+              tx="settingsScreen.referral"
+              textStyle={SETTINGS_MENU.TABLE_CELL_TEXT}
+              style={SETTINGS_MENU.TABLE_CELL_FIRST_CHILD}
+              onPress={this.onPressReferral}
+            />
+          }
         </View>
         {isIAPEnabled &&
           <View style={SETTINGS_MENU.TABLE}>
