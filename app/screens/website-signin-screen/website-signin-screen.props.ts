9000dec2efcbf29ff97b5425d13f2d71460bc168
Ng Wing Tat, David <i@ngwingt.at>
2020-05-19 18:44:38 +0800

    🔧 Update website sign-in links

--- app/screens/website-signin-screen/website-signin-screen.props.ts
+++ app/screens/website-signin-screen/website-signin-screen.props.ts
@@ -15,27 +15,27 @@ export const WEBSITE_SIGNIN_ITEMS: ReadonlyArray<WebsiteSignInItem> = [
   },
   {
     id: "Appledailytw",
-    url: "https://tw.appledaily.com/",
+    url: "https://auth.appledaily.com/web/v7/apps/598aee773b729200504d1f31/login?region=TW&lang=zh_tw",
     logo: require("./icons/applydaily.png"),
   },
   {
     id: "Appledailyhk",
-    url: "https://hk.appledaily.com/",
+    url: "https://auth.appledaily.com/web/v7/apps/598aee533b729200504d1f2e/login?region=HK&lang=zh_hk",
     logo: require("./icons/applydaily.png"),
   },
   {
     id: "Substack",
-    url: "https://substack.com/",
+    url: "https://substack.com/account/login",
     logo: require("./icons/substack.png"),
   },
   {
     id: "Daodu",
-    url: "https://daodu.tech/",
+    url: "https://daodu.tech/wp-login.php",
     logo: require("./icons/daodu.png"),
   },
   {
     id: "Nytimes",
-    url: "https://www.nytimes.com/",
+    url: "https://myaccount.nytimes.com/auth/login?response_type=cookie",
     logo: require("./icons/nytimes.png"),
   },
 ]
