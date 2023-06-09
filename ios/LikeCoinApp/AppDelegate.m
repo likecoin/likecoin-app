2e1a014f8e3b170c43bb40a1e7a026ffdc0d47b2
Ng Wing Tat, David <i@ngwingt.at>
2023-06-10 01:42:29 +0800

    🐛 Remove left over Facebook SDK code

--- ios/LikeCoinApp/AppDelegate.m
+++ ios/LikeCoinApp/AppDelegate.m
@@ -12,7 +12,6 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <React/RCTLinkingManager.h>
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <RNBranch/RNBranch.h>
 
 @import Firebase;
@@ -47,8 +46,7 @@
     FIROptions *options = [[FIROptions alloc] initWithContentsOfFile:filePath];
     [FIRApp configureWithOptions:options];
   }
-  [[FBSDKApplicationDelegate sharedInstance] application:application
-                           didFinishLaunchingWithOptions:launchOptions];
+
 #ifdef DEBUG
   [RNBranch useTestInstance];
 #endif
@@ -68,13 +66,7 @@
 
 - (BOOL)application:(UIApplication *)application openURL:(nonnull NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options {
   return [RNBranch.branch application:application openURL:url options:options]
-  || [[FBSDKApplicationDelegate sharedInstance] application:application
-                                                    openURL:url
-                                          sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
-                                                 annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
-      ] || [RCTLinkingManager application:application
-                                  openURL:url
-                                  options:options];
+    || [RCTLinkingManager application:application openURL:url options:options];
 }
 
 - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
