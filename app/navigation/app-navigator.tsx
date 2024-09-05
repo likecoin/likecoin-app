3f6c7aeeef47ae95ed2c956fa61136a3f28f4279
Ng Wing Tat, David <i@ngwingt.at>
2024-09-05 21:54:00 +0800

    👔 Hide liker.land tabs

--- app/navigation/app-navigator.tsx
+++ app/navigation/app-navigator.tsx
@@ -16,9 +16,6 @@ import { MainTabBarIcon } from "../components/main-tab-bar"
 
 import { ContentViewScreen } from "../screens/content-view-screen"
 import { CrispSupportScreen } from "../screens/crisp-support-screen"
-import { NFTReaderScreen } from "../screens/nft-reader-screen/nft-reader-screen"
-import { NFTNotificationScreen } from "../screens/nft-notification-screen/nft-notification-screen"
-// import { NotificationScreen } from "../screens/notification-screen"
 import { ReferrerFollowScreen } from "../screens/referrer-follow-screen"
 import { QrcodeScannerScreen } from "../screens/qrcode-scanner-screen"
 import { ReceiveScreen } from "../screens/receive-screen"
@@ -29,8 +26,6 @@ import { color } from "../theme"
 
 const MainTabs = createBottomTabNavigator({
   Dashboard: DashboardNavigator,
-  NFT: NFTReaderScreen,
-  Notification: NFTNotificationScreen,
   Settings: SettingsNavigator,
 }, {
   initialRouteName: "Dashboard",
